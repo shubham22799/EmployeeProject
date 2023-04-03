44406305784bfb6ce673946225e136aff4b6db8f
Paymentz <pzapps@paymentz.in>
2023-04-03 15:48:37 +0530

    commit1

---
+++ employeeProject-1/src/test/java/com/spring/employee/EmployeeProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
